ee1d00f7b7b6d8ba664527904b9f5f8e8f7e5a9f
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 13:19:56 +0530

    Extra Operation on list

---
+++ List/List_Other_operation.py
@@ -0,0 +1,27 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Some othe operation of LIST
+"""
+
+list = ["hi", "hehe", "hola"]
+
+# Length of the list
+print(len(list))
+
+# Concatenation of 2 Strings
+list1 = [2,3,4]
+list2 = [5,6]
+list3 = list1 + list2
+print(list3)
+
+# Repetition
+print(list * 2)
+
+# Membership
+print("hi" in list)
+
+# Itertion
+for x in list:
+    print(x)
+
