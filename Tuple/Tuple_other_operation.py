7b9eff0e56bbc4c4bb641f2bf7823c15536529d0
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 15:52:31 +0530

    Tuple Other Operations

---
+++ Tuple/Tuple_other_operation.py
@@ -0,0 +1,25 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Tuples other operation
+"""
+
+tup1 = (1,2,3,4)
+tup2 = ('Hi!',)
+
+# Length operation
+print(tup1)
+
+# Concatenation
+tup3 = tup1 + tup2
+print(tup3)
+
+# Repetition
+print(tup2 * 4)
+
+# Membership
+print( 3 in tup1)
+
+# Iteration
+for x in tup1:
+    print (x)
