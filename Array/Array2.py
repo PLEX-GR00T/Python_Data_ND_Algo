d107e4ae7212ac1d98d02098b3ef352895e83a34
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 11:15:06 +0530

    Accessing an Array

---
+++ Array/Array2.py
@@ -0,0 +1,12 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Accessing Array Element
+
+"""
+from array import *
+
+array2 = array('i', [22,33,55,33,77])
+
+print (array2[0])
+print (array2[1])
