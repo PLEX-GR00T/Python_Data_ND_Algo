d8e83c72477ab1af6998bc4c8e438b7009c6c803
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 16:38:15 +0530

    Traverse in 2d array

---
+++ 2D_Array/Traverse_2D_array.py
@@ -0,0 +1,13 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Accessing 2D Array
+"""
+import sys
+from array import *
+
+T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+for x in T:
+    for y in x:
+        print(y, end = " ")
+    print()
