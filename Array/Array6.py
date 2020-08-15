5e3333842c16ffbb2aaf48f168741fbf21e7cf6a
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 11:30:58 +0530

    Update Operation

---
+++ Array/Array6.py
@@ -0,0 +1,14 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Update Operation in an Array
+
+"""
+from array import *
+
+array6 = array('i',[12,23,34,45,56])
+
+array6[0]= 99
+
+for a in array6:
+    print(a)
