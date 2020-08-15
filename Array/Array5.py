4ce1c2e8c8342ea75712b013b0b89147e25f4410
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 11:26:04 +0530

    Search operation

---
+++ Array/Array5.py
@@ -0,0 +1,11 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Search Operation in an Array
+
+"""
+from array import *
+
+array5 = array('i',[12,23,34,34,45,56]) #It returns the fist occurance location of the element if they are redundant.
+
+print(array5.index(34))
