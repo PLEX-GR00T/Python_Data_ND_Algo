840a3a0473732b5955b55250ee142235caed27b3
PLEX-GR00t <pthakkar16298@gmail.com>
2020-08-15 11:21:42 +0530

    Deletion operation

---
+++ Array/Array4.py
@@ -0,0 +1,15 @@
+"""
+Name : Priyank Thakkar
+Date : 15/8/2020
+Info : Deletion Operation in an Array
+
+"""
+from array import *
+
+array4 = array('i',[53,643,64,23,63])
+
+array4.remove(53) #remove(value)
+
+for x in array4:
+    print(x)
+    
